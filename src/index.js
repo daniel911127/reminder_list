c891712fde767bbadbf4651df6ca6290ae961a5c
daniel911127 <69252410+daniel911127@users.noreply.github.com>
2021-05-03 07:17:10 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
 var restify = require('restify');
 
 var { addUser } = require('./users/add-user');
-var { getUsers } = require('./users/get-user');
+var { getUser } = require('./users/get-user');
+var { deleteUser } = require('./users/delete-user');
+var { updateUser } = require('./users/update-user')
 
 var { addList } = require('./lists/add-list');
-var { getLists } = require('./lists/get-list');
+var { getList } = require('./lists/get-list');
+var { deleteList } = require('./lists/delete-list');
 
 var { addItem } = require('./items/add-item');
-var { getItems } = require('./items/get-item');
-
+var { getItem } = require('./items/get-item');
+var { deleteItem } = require('./items/delete-item');
 
 var server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 
-server.post('/users',getUsers);
-server.get('/users',addUser);
+server.post('/users', addUser);
+server.get('/users/:id_user', getUser);
+server.del('/users/:id_user', deleteUser);
+server.put('/users/:id_user', updateUser);
 
-server.post('/lists',getLists);
-server.get('/lists',addList);
+server.post('/users/:id_user/lists', addList);
+server.get('/users/:id_user/lists/:id_list', getList);
+server.del('/users/:id_user/lists/:id_list', deleteList);
 
-server.listen(8080, function() {
+server.get('/users/:id_user/lists/:id_list/items/:id_message', getItem);
+server.post('/users/:id_user/lists/:id_list/items', addItem);
+server.del('/users/:id_user/lists/:id_list/items/:id_message', deleteItem);
+
+server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
